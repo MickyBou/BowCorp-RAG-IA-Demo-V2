30f2d39d60a1814b4b22481184dc76710851a9bd
MickyBou <mickybou@gmail.com>
2025-02-27 09:46:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ if Model == "GEMINI":
         },
     ]
 
-    model = genai.GenerativeModel(model_name="gemini-pro",
+    model = genai.GenerativeModel(model_name="gemini-2.0-flash",
                                 generation_config=generation_config,
                                 safety_settings=safety_settings)
     
